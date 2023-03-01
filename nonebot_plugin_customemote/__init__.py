654600deef586621976955de237479473d0bc267
DMCSWCG <59276590+DMCSWCG@users.noreply.github.com>
2023-03-01 16:39:36 +0800

    修改Config导入模式

--- nonebot_plugin_customemote/__init__.py
+++ nonebot_plugin_customemote/__init__.py
@@ -8,12 +8,33 @@ except:
     from nonebot.adapters.onebot.v11 import Bot, MessageSegment,GroupMessageEvent,Message
 finally:
     raise ImportError("No support adapter find! Abort load!")
-
+from nonebot.plugin import PluginMetadata
 import time,re
 from data_source import *
 from config import *
 
-customemote = CustomEmote()
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
+
+
+
+__plugin_meta__ = PluginMetadata(
+    name="群聊自定义表情包发送器",
+    description="用于群聊中设置自定义表情包，并在需要的时候使用对应名称召唤机器人发送表情，不再有打开相册翻表情的烦恼",
+    usage="触发方式：指令 + @user/qq/自己/图片\n发送“头像表情包”查看支持的指令",
+    config=Config,
+    extra={
+        "unique_name": "custom_face",
+        "example": "设置表情：自定义表情设置 表情名称\n[回复图片]自定表情包设置 滑稽\n召唤表情：表情名称.jpg\n傻了吧唧的.jpg",
+        "author": "DMCSWCG <xxxxxxx@gmail.com>",
+        "version": "0.1.0",
+    },
+)
+
+
+
+
+customemote = CustomEmote(plugin_config)
 
 custom_emote_image_set = on_command("自定表情包设置",aliases={"自定义表情包设置","自定表情设置","自定义表情设置"},priority=10,block=False)
 @custom_emote_image_set.handle()
